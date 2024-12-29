249415c29dd87389dc3a63392d402752ec450e5c
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2024-12-29 10:26:22 +0530

    Create Count of smaller elements.py

---
+++ Count of smaller elements.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countOfElements(self, x, arr):
+        # Code Here
+        count = 0
+        for i in arr:
+            if i <= x:
+                count+=1
+        return count
