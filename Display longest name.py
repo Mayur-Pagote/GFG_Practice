741cbb7be5446c738e9994b11836404277d63fa2
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2024-12-30 11:58:58 +0530

    Create Display longest name.py

---
+++ Display longest name.py
@@ -0,0 +1,8 @@
+class Solution:
+    def longest(self, names):
+        # code here
+        long_name = names[0]
+        for i in names:
+            if len(i) > len(long_name):
+                long_name = i
+        return long_name
