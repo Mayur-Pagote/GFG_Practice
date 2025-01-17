b4b61373d2f346cc2c0da7708b8794248342f441
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-01-17 17:10:29 +0530

    Create Pairs of Adjacent elements.py

---
+++ Pairs of Adjacent elements.py
@@ -0,0 +1,8 @@
+class Solution:
+    def adjacentPairs(self,arr):
+        # code here
+        count = 0
+        for i in range(len(arr)-1):
+            if arr[i]+1 == arr[i+1]:
+                count += 1
+        return count
