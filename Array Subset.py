e4f7be2057570f6e2bfb9d2a1911bb2221e0d847
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-02-28 18:05:59 +0530

    Update Array Subset.py

--- Array Subset.py
+++ Array Subset.py
@@ -1,10 +1,6 @@
 class Solution:
-    # Function to check if b is a subset of a.
     def isSubset(self, a, b):
-        # Converting list a to a set for faster lookups.
         set_a = set(a)
-        
-        # Check if all elements in b are present in set_a.
         for element in b:
             if element not in set_a:
                 return False
